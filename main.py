b5126d4b8f0e0d4766c3b506ac3e996eeb593caa
tkr0895 <TomKramer95@gmail.com>
2025-02-28 17:39:51 +0100

    refactoring

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import pygame
 from constants import *
 
-def main():
-    pygame.init()
+def startup_print():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+def set_variables_for_game_loop():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    return screen, clock, dt
+
+def game_loop(screen, clock, dt):
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-
+                if event.type == pygame.QUIT:
+                    return
         screen.fill((0, 0, 0))
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
+def main():
+    pygame.init()
+    startup_print()
+    screen, clock, dt = set_variables_for_game_loop()
+    game_loop(screen, clock, dt)
 
 if __name__ == "__main__":
     main()
