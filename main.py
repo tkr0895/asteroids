0b6dfc08139a42d6872376bb652b58dc17cd3d0b
tkr0895 <TomKramer95@gmail.com>
2025-02-28 17:30:56 +0100

    added Clock and tick to main

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -14,8 +16,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
-
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
