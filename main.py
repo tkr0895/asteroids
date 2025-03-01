83feba929087a68742e6bb6db3fd5e24e4b267c3
tkr0895 <TomKramer95@gmail.com>
2025-03-01 09:37:02 +0100

    implement asteroids in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame 
 from constants import *
 from player import *
+from asteroids import *
 
 black = (0, 0, 0)
 
@@ -15,7 +16,7 @@ def set_variables_for_game_loop():
     dt = 0
     return screen, clock, dt
 
-def game_loop(screen, clock, dt, updateables, drawables):
+def game_loop(screen, clock, dt, updateables, drawables, asteroids):
     while True:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -27,19 +28,21 @@ def game_loop(screen, clock, dt, updateables, drawables):
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
-def add_groups_for_Player():
+def add_groups():
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    return updatables, drawables
+    return updatables, drawables, asteroids
 
 def main():
     pygame.init()
     startup_print()
     screen, clock, dt = set_variables_for_game_loop()
-    updatables, drawables = add_groups_for_Player()
-    game_loop(screen, clock, dt, updatables, drawables)
+    updatables, drawables, asteroids = add_groups()
+    game_loop(screen, clock, dt, updatables, drawables, asteroids)
 
 if __name__ == "__main__":
     main()
