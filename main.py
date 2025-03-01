770c3903910fca7b8046ed1828c2534d0ff9c72a
tkr0895 <TomKramer95@gmail.com>
2025-03-01 10:51:23 +0100

    add collision detection for shots

--- main.py
+++ main.py
@@ -18,21 +18,31 @@ def set_variables_for_game_loop():
     dt = 0
     return screen, clock, dt
 
-def check_for_collisions(asteroids, player):
+def check_for_collisions_single(asteroids, other):
     for asteroid in asteroids:
-            collision = asteroid.check_collision(player)
+            collision = asteroid.check_collision(other)
             if collision:
                 print("Game over!")
                 exit()
 
-def game_loop(screen, clock, dt, updateables, drawables, asteroids, player):
+def check_for_collisions_group(asteroids, others):
+     for asteroid in asteroids:
+          for other in others:
+            collision = asteroid.check_collision(other)
+            if collision:
+                print("Collision")
+                asteroid.kill()
+                other.kill()
+
+def game_loop(screen, clock, dt, updateables, drawables, asteroids, player, shots):
     while True:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
         screen.fill(BLACK)
         updateables.update(dt)
-        check_for_collisions(asteroids, player)
+        check_for_collisions_single(asteroids, player)
+        check_for_collisions_group(asteroids, shots)
         for drawable in drawables:
             drawable.draw(screen)
         pygame.display.flip()
@@ -45,21 +55,21 @@ def add_groups():
     shots = pygame.sprite.Group()
     return updatables, drawables, asteroids, shots
 
-def add_containers(updatables, drawables, asteroids):
+def add_containers(updatables, drawables, asteroids, shots):
     Player.containers = (updatables, drawables)
     Asteroid.containers = (asteroids, updatables, drawables)
     AsteroidField.containers = (updatables,)
-    Shot.containers = (updatables, drawables)
+    Shot.containers = (updatables, drawables, shots)
 
 def main():
     pygame.init()
     startup_print()
     screen, clock, dt = set_variables_for_game_loop()
     updatables, drawables, asteroids, shots = add_groups()
-    add_containers(updatables, drawables, asteroids)
+    add_containers(updatables, drawables, asteroids, shots)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
-    game_loop(screen, clock, dt, updatables, drawables, asteroids, player)
+    game_loop(screen, clock, dt, updatables, drawables, asteroids, player, shots)
 
 if __name__ == "__main__":
     main()
